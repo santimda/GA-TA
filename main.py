3fbfb4f95e97e90d376be01f6d9d3715bcb6bbe9
mmgamboa <martingamboa2@gmail.com>
2019-01-04 14:59:41 -0300

    solo ejecuta lo que se introdujo por linea de comandos. No opera ni genera archivos de salida. Todo esto está dentro de cada clase.

--- main.py
+++ main.py
@@ -11,14 +11,13 @@ from Structure_structure import Structure
 #read the table and return a data with many atributes
 
 if len(sys.argv) == 3:
-	argum = set(sys.argv[1])
+	argum = set(sys.argv[1].lower())
 	data = Data(sys.argv[2])
 else:
 	argum = ' '
 	data = Data(sys.argv[1]) 
-info = True#False
 
-if info:
+if data.info:
 	print '{0} file contains: {1} total number of persons in study, with {2} womens and {3} mens.'.format(
 	sys.argv[1], data.total_MenWomen, data.n_women, data.n_men)
 	print 'The investigation has {0} subpopulations with: {1} persons each one'.format(data.totalPopulations, data.n_each_population)
@@ -31,32 +30,13 @@ if info:
 	print 'filevalues shape', np.shape(data.fileValues)
 
 if 'r' in argum:
-
-	rData = R(data)
-	output_name_R = 'outputR'
-	np.savetxt(output_name_R+'.txt', rData.data, fmt='%4d', header = rData.header, comments = '')
-	# convert .txt in a spreadsheet
-	os.system('ssconvert '+output_name_R+'.txt '+output_name_R+'.xlsx')
+	R(data)
 
 if 'a' in argum: 
-
-	arlequinData = Arlequin(data)
-	output_name_arlequin = 'outputArlq'
-	
-	ArrFmt = ['%s']
-	for i in range(0,(np.shape(arlequinData.data)[1]-1)):
-		ArrFmt.append('%4d')
-	np.savetxt(output_name_arlequin+'.txt', arlequinData.data, comments = '', fmt=ArrFmt)#, header = arlequinData.header)
-	os.system('ssconvert '+output_name_arlequin+'.txt '+output_name_arlequin+'.xlsx')
-
+	Arlequin(data)
 
 if 's' in argum:
-
-	structureData = Structure(data)
-	output_name_structure = 'outputStr'
-
-	np.savetxt(output_name_structure+'.txt', structureData.data, comments = '', fmt='%4s')#, header = arlequinData.header)
-	os.system('ssconvert '+output_name_structure+'.txt '+output_name_structure+'.xlsx')
+	Structure(data)
 
 if not 'r' in argum or not 'a' in argum or not 's' in argum: 
 	ValueError('You have to specify r (for R), a (for Arlequin) or s (for Structure) parameter.')
