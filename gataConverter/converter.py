6eea028aadb6eee7f6c21f72fce1db9bc4ad1a7e
Santiago Tettamanti <santi.tettamanti96@gmail.com>
2019-03-23 20:52:49 -0300

    Agregué texto y los checkbox del formato de salida al front. Empecé la conexión de la app con los scripts a traves de una clase nueva Converter

---
+++ gataConverter/converter.py
@@ -0,0 +1,6 @@
+from gataConverter.scripts.readtable import Data
+class Converter:
+
+	def checkData(filename):
+		Data(filename)
+
--- gataConverter/views.py
+++ gataConverter/views.py
@@ -3,13 +3,14 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template import loader
 from django.core.files.storage import FileSystemStorage
+from gataConverter.converter import Converter
 
 # Create your views here.
 def index(request):
     if request.method == 'POST':   
         file = request.FILES['customFile']
-        handle_uploaded_file(file)
-        return download(file.name)
+        #Converter.checkData(handle_uploaded_file(file))
+        return download(file)
    
     return render(request, 'gataConverter/index.html',)
 
@@ -18,11 +19,11 @@ def handle_uploaded_file(file):
     print(file.name)
     filename = fs.save(file.name, file)
     uploaded_file_url = fs.url(filename)
+    return filename
 
-def download(file_path):
-    if os.path.exists(file_path):
-        with open(file_path, 'rb') as fh:
-            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
-            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
-            return response
-    raise Http404
+
+def download(file):
+    print(file.name)
+    response = HttpResponse(file, content_type="application/vnd.ms-excel")
+    response['Content-Disposition'] = 'inline; filename=' + file.name
+    return response
